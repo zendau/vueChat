80350781e5fee95de207c0fd53b52f2fe3c03bd9
zendau <cowalyow2012@gmail.com>
2021-06-06 00:06:13 +0300

    started develop vue chat app with express and socket.io

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+
+const io = require('socket.io')(http, {
+    cors: {
+      credentials: true
+    }
+})
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.get('/', (req, res) => {
+  io.emit("message", "test")  
+  res.json("ok")
+});
+
+
+// Вывод сообщение что был подключен пользователь по сокету
+io.on("connection", (socket) => {
+    console.log("user connect")
+})
+
+
+const PORT = process.env.PORT || 80
+
+const ifaces = require('os').networkInterfaces();
+    const localhost = Object.keys(ifaces).reduce((host,ifname) => {
+        let iface = ifaces[ifname].find(iface => !('IPv4' !== iface.family || iface.internal !== false));
+        return iface? iface.address : host;
+    }, '127.0.0.1');
+    http.listen(PORT, () => {
+        console.log(`server started on http://${localhost}:${PORT}`)
+})
---
+++ frontend/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
+<script>
+
+import {io} from "socket.io-client"
+import {onMounted} from "vue"
+
+export default {
+
+  setup() {
+
+    onMounted(() => {
+      const socket = io("http://172.21.112.1/")
+      socket.on('message', (data) => {
+        console.log(data)
+      })
+    })
+
+
+    return
+  }
+}
+</script>
